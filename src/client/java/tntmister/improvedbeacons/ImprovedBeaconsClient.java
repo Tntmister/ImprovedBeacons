569c3c60000670323f7f6c4cf5187f8627d50caa
Tntmister <diogoamorim_2011@hotmail.com>
2026-01-29 18:43:59 +0000

    remove kotlin

---
+++ src/client/java/tntmister/improvedbeacons/ImprovedBeaconsClient.java
@@ -0,0 +1,10 @@
+package tntmister.improvedbeacons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImprovedBeaconsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
