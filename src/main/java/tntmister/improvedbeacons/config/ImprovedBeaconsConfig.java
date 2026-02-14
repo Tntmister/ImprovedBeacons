4692dd51c32989dd2c8f6cfc3d539915179fdc92
Tntmister <diogoamorim_2011@hotmail.com>
2026-02-14 14:16:06 +0000

    port config screen to 1.21.11

--- src/main/java/tntmister/improvedbeacons/config/ImprovedBeaconsConfig.java
+++ src/main/java/tntmister/improvedbeacons/config/ImprovedBeaconsConfig.java
@@ -4,11 +4,11 @@ import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public class ImprovedBeaconsConfig {
     public static ConfigClassHandler<ImprovedBeaconsConfig> HANDLER = ConfigClassHandler.createBuilder(ImprovedBeaconsConfig.class)
-            .id(ResourceLocation.fromNamespaceAndPath("improved-beacons", "improved-beacons"))
+            .id(Identifier.fromNamespaceAndPath("improved-beacons", "improved-beacons"))
             .serializer(config -> GsonConfigSerializerBuilder.create(config)
                     .setPath(FabricLoader.getInstance().getConfigDir().resolve("improved-beacons.json"))
                     .build())
